efba45f3476dde553243d14bbf2c77d7a37feea6
rtprashant <151728629+rtprashant@users.noreply.github.com>
2025-01-21 00:16:10 +0530

    Update chatBot.js

--- api/chatBot.js
+++ api/chatBot.js
@@ -1,7 +1,7 @@
 import api from "./api"
 
 const chatBot = async(data)=>{
-    const response = await api.post(`/chatBot` , data)
+    const response = await api.post(`/api/v1/chat/chatBot` , data)
     return response.data
 
 }
