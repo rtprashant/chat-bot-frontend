65e05d28f0ab06554640e82dd385ee7141033cd1
prashant <prashantrajput827327@gamil.com>
2025-01-28 07:25:04 +0530

    api time increase

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    timeout: 5000, 
+    timeout: 10000, 
     withCredentials: true, 
     headers: {
       'Content-Type': 'application/json',
